8f0cc6fed2b3b22cb8d8daffb5bedce2a9f2ea85
Know Group <>
2018-12-10 16:15:40 +0530

    0001- Initial Setup

---
+++ src/com/know/lambda/Lesson_00_Lambda_Expression.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.know.lambda;
+
+/**
+ * Lambda Expressions are Anonymous Function,
+ * That is function with
+ *  - NO NAME
+ *  - NO RETURN TYPE
+ *  - NO MODIFIER
+ * 
+ * Lambda expression are introduced in java 8 to 
+ * enable Functional Programming features in java.
+ * Other Languages like Python, R, C , C++ 
+ * already have Lambda Features.
+ * 
+ * 
+ * @author KnowGroup
+ */
+public class Lesson_00_Lambda_Expression {
+    
+}
---
+++ src/com/know/lambda/Lesson_01_FuntionalInterace.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.know.lambda;
+
+/**
+ * Functional Interface (FI) are java interface 
+ * containing Single Abstract Method (SAM)
+ * 
+ * @FunctionalInterface can be used to validate FI at compile time.
+ * 
+ * - FI can have any number of default or static 
+ * method but they must have SAM.
+ * 
+ * e.g. Runnable , Callable , Comparable 
+ * 
+ * @author KnowGroup
+ */
+public class Lesson_01_FuntionalInterace {
+    
+}
